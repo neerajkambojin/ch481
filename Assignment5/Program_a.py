d58ffb4fb9a1e8f77b889464cff8508c023070e6
neeraj <neerajkamboj101@gmail.com>
2022-02-02 23:09:38 +0530

    Assignment5 program_a rough

--- Assignment5/Program_a.py
+++ Assignment5/Program_a.py
@@ -1,1 +1,24 @@
-print('Program_a')
+# f(x) = e^(-x) + 4x^3 - 5 = 0, x1 = 1 and x2 = 2
+import numpy as np
+
+x1 = 1
+x2 = 2
+
+tol_limit = 10 ** (-6)
+
+
+def fun(x):
+    return np.exp(-x) + (4 * (x ** 3)) - 5
+
+
+while abs(x1 - x2) / 2 >= tol_limit:
+    x3 = (x1 + x2) / 2
+    if fun(x1) * fun(x3) == 0 or fun(x2) * fun(x3) == 0:
+        break
+    if fun(x1) * fun(x3) <= 0:
+        x2 = x3
+    else:
+        x1 = x3
+
+print(x3)
+print(fun(x3))
