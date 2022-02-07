a9ca70ecaf19e25b2aefa7a337f2597cc6f62833
neeraj <neerajkamboj101@gmail.com>
2022-02-08 02:28:06 +0530

    minor change

--- Assignment5/Program_a.py
+++ Assignment5/Program_a.py
@@ -2,13 +2,17 @@
 
 import numpy as np
 
-x1 = 1; x2 = 2; tol_limit = 10 ** (-6)
+x1 = 1
+x2 = 2
+tol_limit = 10 ** (-6)
 iterative_values = []
 
-def fun(x):
-    return np.exp(-x) + (4 * (x ** 3)) - 5 # Function for calculating value of polynomial for given x
 
-while abs(x1 - x2) >= 2*tol_limit: # Setting tolerance limit
+def fun(x):
+    return np.exp(-x) + (4 * (x ** 3)) - 5  # Function for calculating value of polynomial for given x
+
+
+while abs(x1 - x2) >= 2 * tol_limit:  # Setting tolerance limit
     x3 = (x1 + x2) / 2
     iterative_values.append(x3)
     if fun(x3) == 0:
