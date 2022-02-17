028d1b58a3f698f97bcf8a4187ad6436e59fadb7
neeraj <neerajkamboj101@gmail.com>
2022-02-17 15:00:06 +0530

    Practice Problems

---
+++ Practice Problems/p1.py
@@ -0,0 +1,13 @@
+# To assign grades
+
+marks = float(input("Enter marks: "))
+if marks >= 95:
+    print("AP")
+elif 95 > marks >= 85:
+    print("AA")
+elif 85 > marks >= 75:
+    print("AB")
+elif 75 > marks >= 65:
+    print("BB")
+else:
+    print("Tu to gya!!! Khtm tata bye bye")
