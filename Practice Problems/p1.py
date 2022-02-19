b05fbd1d96783819916791ca8c9271d7e1842ff2
neeraj <neerajkamboj101@gmail.com>
2022-02-19 14:35:28 +0530

    Minor change

--- Practice Problems/p1.py
+++ Practice Problems/p1.py
@@ -1,7 +1,9 @@
 # To assign grades
 
 marks = float(input("Enter marks: "))
-if marks >= 95:
+while marks > 100:
+    marks = float(input("Enter valid marks: "))
+if 100 >= marks >= 95:
     print("AP")
 elif 95 > marks >= 85:
     print("AA")
