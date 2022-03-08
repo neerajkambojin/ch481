395029d1c5517fe8dd543eb1ba60c3a993dc8739
neeraj <neerajkamboj101@gmail.com>
2022-03-09 01:29:40 +0530

    del matrix44.py

---
+++ Assignment7/rough.py
@@ -0,0 +1,12 @@
+# # Error handling
+# try:
+#     number = int(input("Number: "))
+#     print(number)
+#
+# except ValueError:
+#     print("Enter integer only! ")
+#     number = int(input("Number: "))
+name = "Shain"
+fav_food = "Protein Shake"
+print(f"The sum of 3 and 2 is {3+2} dsbivhasdiuh", 3 + 2)
+print(f"My name is {name} and i like {fav_food}")
