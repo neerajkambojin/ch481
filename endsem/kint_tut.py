3a9c10b73f178c0c1a113646150f8be705297dd0
neeraj <neerajkamboj101@gmail.com>
2022-03-30 08:59:15 +0530

    TKINTER

---
+++ endsem/kint_tut.py
@@ -0,0 +1,24 @@
+from tkinter import *
+
+root = Tk()
+
+name = Label(root, text="Neeraj Kamboj")
+name2 = Label(root, text="Mangala")
+
+name.pack()
+name2.pack()
+
+in_box = Entry(root, bg="lightgreen")
+in_box.pack()
+in_box.insert(0, "Coordinates")
+
+
+def greet():
+    out = Label(root, text=f"You typed:, {in_box.get()}")
+    out.pack()
+
+
+in_button = Button(root, text="Enter coordinates", command=greet, padx=30, pady=10)
+in_button.pack()
+
+root.mainloop()
