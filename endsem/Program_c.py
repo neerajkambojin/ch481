52ba0652f8dea2a9e2de20f55b9ef4cd5a22517b
neeraj <neerajkamboj101@gmail.com>
2022-04-17 15:43:04 +0530

    Cleared Up

--- endsem/Program_c.py
+++ endsem/Program_c.py
@@ -1,196 +1,65 @@
 import numpy as np
-
-g_space = np.zeros((5, 5), object)
-g_space[0, 2] = "PA"
-g_space[4, 2] = "PB"
-g_space[1, 1] = "EA"
-g_space[1, 4] = "EA"
-g_space[3, 0] = "EB"
-g_space[3, 3] = "EB"
-
-for i in range(5):
-    for j in range(5):
-        if g_space[i, j] == 0:
-            g_space[i, j] = "--"
-
-ae1, ae2 = 11, 14
-be1, be2 = 30, 33
-a_positions = np.array([ae1, ae2])
-b_positions = np.array([be1, be2])
-p_positions = np.array([2, 42])
-
-while True:
-    print("*********AAA*********")
-    e_positions = np.append(a_positions, b_positions)
-    occ_positions = np.append(e_positions, p_positions)
-    print(g_space)
-    x_a = int(input("Player A move from: "))
-    while x_a not in a_positions:
-        x_a = int(input("Select valid start: "))
-
-    rest_es = list(e_positions)
-    rest_es.remove(x_a)
-    rest_es = np.array(rest_es)
-    restricted_positions = np.empty(0)
-    for i in rest_es:
-        restricted_positions = np.append(restricted_positions, np.linspace(i - 1, i + 1, 3))
-        restricted_positions = np.append(restricted_positions, np.linspace(i - 10, i + 10, 3))
-    restricted_positions = np.append(restricted_positions, occ_positions)
-
-    valid_moves = np.linspace(x_a - 2, x_a + 2, 5)
-    valid_moves = np.append(valid_moves, np.linspace(x_a - 20, x_a + 20, 5))
-    risk_moves = np.linspace(x_a - 2, x_a + 2, 3)
-    risk_moves = np.append(risk_moves, np.linspace(x_a - 20, x_a + 20, 3))
-    win_positions = np.array([41, 43, 32])
-    y_a = input("Move to: ")
-    while True:
-        if int(y_a) in valid_moves and int(y_a) not in restricted_positions:
-            if int(y_a[0]) > 4 or int(y_a[0]) < 0 or int(y_a[1]) > 4 or int(y_a[1]) < 0:
-                print("Illegal move!!!")
-                y_a = int(input("Move to: "))
-            else:
-                break
-        else:
-            print("Illegal move!!!")
-            y_a = input("Move to: ")
-
-    if int(y_a) in risk_moves:
-        prob = np.random.random(1)
-        if prob > 0.5:
-            print("Move rejected!")
-            print(g_space)
-        else:
-            print("Move accepted!")
-            if x_a == a_positions[0]:
-                a_positions[0] = y_a
-            else:
-                a_positions[1] = y_a
-            f_index = tuple(y_a)
-            i_index = tuple(str(x_a))
-            i_index = list(map(int, i_index))
-            f_index = list(map(int, f_index))
-            g_space[i_index[0], i_index[1]] = "--"
-            g_space[f_index[0], f_index[1]] = "EA"
-            print(g_space)
-    else:
-        if x_a == a_positions[0]:
-            a_positions[0] = y_a
-        else:
-            a_positions[1] = y_a
-        f_index = tuple(y_a)
-        i_index = tuple(str(x_a))
-        i_index = list(map(int, i_index))
-        f_index = list(map(int, f_index))
-        g_space[i_index[0], i_index[1]] = "--"
-        g_space[f_index[0], f_index[1]] = "EA"
-        print(g_space)
-    if int(y_a) in win_positions:
-        print("A wins!!!")
-        break
-
-
-    e_positions = np.append(a_positions, b_positions)
-    occ_positions = np.append(e_positions, p_positions)
-    x_b = int(input("Player B move from: "))
-    while x_b not in b_positions:
-        x_b = int(input("Select valid start: "))
-
-    rest_es = list(e_positions)
-    rest_es.remove(x_b)
-    rest_es = np.array(rest_es)
-    restricted_positions = np.empty(0)
-    for i in rest_es:
-        restricted_positions = np.append(restricted_positions, np.linspace(i - 1, i + 1, 3))
-        restricted_positions = np.append(restricted_positions, np.linspace(i - 10, i + 10, 3))
-    restricted_positions = np.append(restricted_positions, occ_positions)
-
-    valid_moves = np.linspace(x_b - 2, x_b + 2, 5)
-    valid_moves = np.append(valid_moves, np.linspace(x_b - 20, x_b + 20, 5))
-    risk_moves = np.linspace(x_b - 2, x_b + 2, 3)
-    risk_moves = np.append(risk_moves, np.linspace(x_b - 20, x_b + 20, 3))
-    win_positions = np.array([1, 3, 12])
-    y_b = input("Move to: ")
-    while True:
-        if int(y_b) in valid_moves and int(y_b) not in restricted_positions:
-            if int(y_b[0]) > 4 or int(y_b[0]) < 0 or int(y_b[1]) > 4 or int(y_b[1]) < 0:
-                print("Illegal move!!!")
-                y_b = int(input("Move to: "))
-            else:
-                break
-        else:
-            print("Illegal move!!!")
-            y_b = input("Move to: ")
-
-    if int(y_b) in risk_moves:
-        prob = np.random.random(1)
-        if prob > 0.5:
-            print("Move rejected!")
-            print(g_space)
-        else:
-            print("Move accepted!")
-            if x_b == a_positions[0]:
-                a_positions[0] = y_b
-            else:
-                a_positions[1] = y_b
-            f_index = tuple(y_b)
-            i_index = tuple(str(x_b))
-            i_index = list(map(int, i_index))
-            f_index = list(map(int, f_index))
-            g_space[i_index[0], i_index[1]] = "--"
-            g_space[f_index[0], f_index[1]] = "EB"
-            print(g_space)
-    else:
-        if x_b == b_positions[0]:
-            b_positions[0] = y_b
-        else:
-            b_positions[1] = y_b
-        f_index = tuple(y_b)
-        i_index = tuple(str(x_b))
-        i_index = list(map(int, i_index))
-        f_index = list(map(int, f_index))
-        g_space[i_index[0], i_index[1]] = "--"
-        g_space[f_index[0], f_index[1]] = "EB"
-        print(g_space)
-    if int(y_b) in win_positions:
-        print("B wins!!!")
-        break
-
-
-'''
-import numpy as np
 import time
-from tabulate import tabulate   # pip install tabulate
+from tabulate import tabulate  # pip install tabulate ( For formatting table)
+from colorama import init, Fore, Back, Style  # pip install colorama ( For color highlighting)
 
 
-def formatter(string):
+# Filtering out undesired elements from input
+def fmt(string):
     digits = "1234567890"
     for x in string:
         if x not in digits:
             string = string.replace(x, "")
-    return string
+    return int(string)
 
 
+# Coordinate to index
+def indexes(ind):
+    ind = list(int(x) for x in str(ind))
+    if len(ind) == 1:
+        i1 = 0
+        i2 = ind[0]
+    else:
+        i1 = ind[0]
+        i2 = ind[1]
+    return i1, i2
+
+
+init()  # Initializing colorama
+
+# Generating board and assigning starting positions to electrons and protons
 g_space = np.empty((5, 5), object)
-g_space[0, 2] = "P(A)"
-g_space[4, 2] = "P(B)"
-g_space[1, 1] = "E(A)"
-g_space[1, 4] = "E(A)"
-g_space[3, 0] = "E(B)"
-g_space[3, 3] = "E(B)"
+g_space[0, 2] = f"{Style.BRIGHT}{Fore.LIGHTCYAN_EX}{Back.MAGENTA}-P(A)(0, 2)-{Style.RESET_ALL}"
+g_space[4, 2] = f"{Style.BRIGHT}{Fore.LIGHTCYAN_EX}{Back.MAGENTA}-P(B)(4, 2)-{Style.RESET_ALL}"
+g_space[1, 1] = f"{Style.BRIGHT}{Fore.GREEN}-E(A)(1, 1)-{Style.RESET_ALL}"
+g_space[1, 4] = f"{Style.BRIGHT}{Fore.GREEN}-E(A)(1, 4)-{Style.RESET_ALL}"
+g_space[3, 0] = f"{Style.BRIGHT}{Fore.RED}-E(B)(3, 0)-{Style.RESET_ALL}"
+g_space[3, 3] = f"{Style.BRIGHT}{Fore.RED}-E(B)(3, 3)-{Style.RESET_ALL}"
 
+# Filling empty positions
 for j in range(5):
     for k in range(5):
         if g_space[j, k] is None:
-            g_space[j, k] = f"--"
+            g_space[j, k] = f"{Style.DIM}{Fore.YELLOW}-{j, k}-{Style.RESET_ALL}"
 
+# Generating array containing all coordinates of board
+tables_values = np.empty(0)
+for x in range(0, 50, 10):
+    for y in range(5):
+        tables_values = np.append(tables_values, x + y)
+
+# Making list containing initial positions of electrons and protons
 a_positions = np.array([11, 14])
 b_positions = np.array([30, 33])
 p_positions = np.array([2, 42])
 
+# Players list for use in switching players
 players = ["A", "B"]
-print(f"\nTable: \n{tabulate(g_space, tablefmt = 'fancy_grid')}\n")
+print(f"\nTable: \n{tabulate(g_space, tablefmt='fancy_grid')}\n")
+
+# Starting main loop
 while True:
-    player = players[0]
+    player = players[0]  # Selecting player
     if player == "A":
         o_positions = a_positions.copy()
         win_positions = np.array([41, 43, 32])
@@ -199,115 +68,130 @@ while True:
         o_positions = b_positions.copy()
         win_positions = np.array([1, 3, 12])
         electron = "E(B)"
-
-    e_positions = np.append(a_positions, b_positions)
-    occ_positions = np.append(e_positions, p_positions)
-
-    while True:
-        try:
-            st_str = formatter(input(
-                f"Player {player} starting coordinates (Valid input format e.g. 14, (1,4), 1 4, '1, 4'): "))
-            st_int = int(st_str)
-            break
-        except ValueError:
-            print("Enter valid coordinates!!!")
-            pass
-
-    while st_int not in o_positions:
-        print("Enter valid coordinates!!!")
+    e_positions = np.append(a_positions, b_positions)  # electron positions
+    occ_positions = np.append(e_positions, p_positions)  # all occupied positions
+    st_cor = 55  # Initializing starting coordinates variable
+    while st_cor not in o_positions:  # checking if electron coordinate is valid
         while True:
-            try:
-                st_str = formatter(input(
-                    f"Player {player} starting coordinates (Valid input format e.g. 14, (1,4), 1 4, '1, 4'): "))
-                st_int = int(st_str)
+            try:  # try except condition to avoid crashing due to ValueError
+                st_cor = fmt(input(f"Player {player} starting coordinates(ij; i,j; i j; (i,J) etc): "))
                 break
             except ValueError:
                 print("Enter valid coordinates!!!")
 
-    rest_es = list(e_positions)
-    rest_es.remove(st_int)
-    rest_es = np.array(rest_es)
-    restricted_positions = np.empty(0)
+    rest_es = e_positions[e_positions != st_cor]  # electrons except the selected one
 
+    # generating array of positions next to electrons
+    restricted_positions = np.empty(0).astype(int)
     for i in rest_es:
         restricted_positions = np.append(restricted_positions, np.linspace(i - 1, i + 1, 3))
         restricted_positions = np.append(restricted_positions, np.linspace(i - 10, i + 10, 3))
 
+    # array containing forbidden positions
     restricted_positions = np.append(restricted_positions, occ_positions)
 
-    valid_moves = np.append(np.linspace(st_int - 2, st_int + 2, 5), np.linspace(st_int - 20, st_int + 20, 5))
-    risk_moves = np.append(np.linspace(st_int - 2, st_int + 2, 3), np.linspace(st_int - 20, st_int + 20, 3))
+    # array for valid moves
+    valid_moves = np.append(np.linspace(st_cor - 2, st_cor + 2, 5), np.linspace(st_cor - 20, st_cor + 20, 5)).astype(int)
+    # array for moves with step = +-2
+    risk_moves = np.append(np.linspace(st_cor - 2, st_cor + 2, 3), np.linspace(st_cor - 20, st_cor + 20, 3)).astype(int)
+
+    # highlighting allowed moves
+    for pos in valid_moves:
+        if pos in risk_moves:
+            if (pos in tables_values) and (pos not in restricted_positions):
+                i1, i2 = indexes(pos)
+                g_space[i1, i2] = f"{Style.BRIGHT}{Fore.MAGENTA}-{i1, i2}-{Style.RESET_ALL}"
+        else:
+            if (pos in tables_values) and (pos not in restricted_positions):
+                i1, i2 = indexes(pos)
+                g_space[i1, i2] = f"{Style.BRIGHT}{Fore.CYAN}-{i1, i2}-{Style.RESET_ALL}"
+    print("Table\nAllowed moves are highlighted :)")
+    print(tabulate(g_space, tablefmt='fancy_grid'))
 
     while True:
         try:
-            fp_str = formatter(input("Move to: "))
-            fp_int = int(fp_str)
+            f_cor = fmt(input("Move to: "))
             break
         except ValueError:
             print("Illegal Move!!!")
             pass
 
     while True:
-        if fp_int in valid_moves and fp_int not in restricted_positions and len(fp_str) == 2:
-            if int(fp_str[0]) > 4 or int(fp_str[0]) < 0 or int(fp_str[1]) > 4 or int(fp_str[1]) < 0:
-                print("Illegal move!!!")
-                while True:
-                    try:
-                        fp_str = formatter(input("Move to: "))
-                        fp_int = int(fp_str)
-                        break
-                    except ValueError:
-                        print("Illegal Move!!!")
-                        pass
-            else:
-                break
+        # condition to avoid invalid move
+        if (f_cor in valid_moves) and (f_cor not in restricted_positions) and (f_cor in tables_values):
+            break
         else:
             print("Illegal move!!!")
             while True:
                 try:
-                    fp_str = formatter(input("Move to: "))
-                    fp_int = int(fp_str)
+                    f_cor = fmt(input("Move to: "))
                     break
                 except ValueError:
                     print("Illegal Move!!!")
                     pass
 
-    prob = np.random.random(1)
-    if (fp_int in risk_moves) and prob >= 0.5:
+    prob = np.random.random(1)  # for probability
+    if (f_cor in risk_moves) and prob <= 0.5:  # for forbidden move
         print("\nTossing the coin!")
         time.sleep(1)
         print("Bad luck, move rejected :(")
-        print(f"\nTable: \n{tabulate(g_space, tablefmt='fancy_grid')}\n")
+
+        # changing colors
+        for pos in o_positions:
+            i1, i2 = indexes(pos)
+            g_space[i1, i2] = f"{Style.BRIGHT}{Fore.RED}-{electron}{i1, i2}-{Style.RESET_ALL}"
+        if player == "A":
+            for pos in b_positions:
+                i1, i2 = indexes(pos)
+                g_space[i1, i2] = f"{Style.BRIGHT}{Fore.GREEN}-E(B){i1, i2}-{Style.RESET_ALL}"
+        else:
+            for pos in a_positions:
+                i1, i2 = indexes(pos)
+                g_space[i1, i2] = f"{Style.BRIGHT}{Fore.GREEN}-E(A){i1, i2}-{Style.RESET_ALL}"
     else:
-        if (prob < 0.5) and (fp_int in risk_moves):
+        if (prob > 0.5) and (f_cor in risk_moves):  # for allowed move
             print("\nTossing the coin!")
             time.sleep(1)
             print("Yay, move accepted :)")
-        if st_int == o_positions[0]:
-            o_positions[0] = fp_str
+        if st_cor == o_positions[0]:
+            o_positions[0] = f_cor
         else:
-            o_positions[1] = fp_str
-        i_index = tuple(st_str)
-        f_index = tuple(fp_str)
-        i_index = list(map(int, i_index))
-        f_index = list(map(int, f_index))
-        g_space[i_index[0], i_index[1]] = f"--"
-        g_space[f_index[0], f_index[1]] = electron
-        print(f"\nTable: \n{tabulate(g_space, tablefmt='fancy_grid')}\n")
-        if fp_int in win_positions:
+            o_positions[1] = f_cor
+
+        # moving electron
+        for pos in o_positions:
+            i1, i2 = indexes(pos)
+            g_space[i1, i2] = f"{Style.BRIGHT}{Fore.RED}-{electron}{i1, i2}-{Style.RESET_ALL}"
+        i1, i2 = indexes(st_cor)
+        g_space[i1, i2] = f"{Style.DIM}{Fore.YELLOW}-{i1, i2}-{Style.RESET_ALL}"
+
+        # changing colors
+        if player == "A":
+            a_positions = np.copy(o_positions)
+            for pos in b_positions:
+                i1, i2 = indexes(pos)
+                g_space[i1, i2] = f"{Style.BRIGHT}{Fore.GREEN}-E(B){i1, i2}-{Style.RESET_ALL}"
+        else:
+            b_positions = np.copy(o_positions)
+            for pos in a_positions:
+                i1, i2 = indexes(pos)
+                g_space[i1, i2] = f"{Style.BRIGHT}{Fore.GREEN}-E(A){i1, i2}-{Style.RESET_ALL}"
+
+        # condition for winning
+        if f_cor in win_positions:
             print(f"Player {player} wins!!!")
             break
 
-    if player == "A":
-        a_positions = np.copy(o_positions)
-    else:
-        b_positions = np.copy(o_positions)
+    e_positions = np.append(a_positions, b_positions)
+    occ_positions = np.append(e_positions, p_positions)
 
+    # resetting colors of unoccupied positions
+    for j in range(5):
+        for k in range(5):
+            if (10 * j) + k not in occ_positions:
+                g_space[j, k] = f"{Style.DIM}{Fore.YELLOW}-{j, k}-{Style.RESET_ALL}"
+    print(f"\nTable: \n{tabulate(g_space, tablefmt='fancy_grid')}\n")
+
+    # changing player
     players.append(player)
     players.pop(0)
-
-'''
-
-'''
-
-'''
